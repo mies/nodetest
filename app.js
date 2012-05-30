c194d5caee0a51ceb7762f74497e0ff1b5919e4a
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-05-30 13:12:44 +0200

    #staging

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.configure(function() {
+  'use strict';
   app.use(express.bodyParser());
   app.use(app.router);
 });
 
 app.get('/', function(req, res) {
+  'use strict';
   res.send({'hello' : 'world'});
 });
 
 var port = process.env.PORT || 3000 ;
 
 app.listen(port, function() {
+  'use strict';
   console.log('listening on port ' + port);
 });
--- test/basictest.js
+++ test/basictest.js
@@ -1,4 +1,5 @@
 exports.testSomethingElse = function(test) {
+  'use strict';
     test.expect(1);
     test.ok(1, "this assertion should FAIL!!");
     test.done();
